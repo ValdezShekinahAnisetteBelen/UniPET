e180cbd6740ab556d44f6578f2fd49764bc4e9b5
packetneptuneofficial <130025682+packetneptuneofficial@users.noreply.github.com>
2023-11-01 23:31:27 +0800

    commit

---
+++ frontend/src/views/EditProduct.vue
@@ -0,0 +1,14 @@
+<template>
+    <EditForm />
+   </template>
+   <script>
+   import EditForm from "@/components/EditForm.vue";
+   export default {
+    name: "EditProduct",
+    components: {
+    EditForm,
+    },
+   };
+   </script>
+   <style>
+   </style>
