33ef5723777621873711972b64caca1ba9d00605
MazoJaylord23 <144674924+MazoJaylord23@users.noreply.github.com>
2023-11-02 10:51:36 +0800

    commit
    registration form

---
+++ frontend/src/components/RegisterForm.vue
@@ -0,0 +1,93 @@
+<template>
+      <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo">
+                <img src="" alt="">
+              </div>
+              <h4>New here? Register Now</h4>
+              <h6 class="font-weight-light"> It only takes a few steps to Register</h6>
+              <form class="pt-3">
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
+                </div>
+            
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="mb-4">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">
+                      I agree to all Terms & Conditions
+                    </label>
+                  </div>
+                </div>
+                <div class="mt-3">
+                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">REGISTER</a>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Already have an account? <a href="login.html" class="text-primary">Login</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+h4, h6 {
+  text-align: center;
+}
+
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
+  text-align: center;
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 20px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+</style>
---
+++ frontend/src/views/Registration.vue
@@ -0,0 +1,20 @@
+<template>
+    <RegisterForm />
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import RegisterForm from '../components/RegisterForm';
+
+  
+  export default defineComponent({
+    name: 'RegisterForm',
+  
+    components: {
+    RegisterForm
+}
+  });
+  </script>
+  
