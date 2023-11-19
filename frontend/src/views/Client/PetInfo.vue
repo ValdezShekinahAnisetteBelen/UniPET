df29e85fffb1008685d80c5ca68176e80b9b4e01
Valdez Shekinah Anisette Belen <shekinahvaldez2003@gmail.com>
2023-11-20 01:48:09 +0800

    fixing log out session
    commit

--- frontend/src/views/Client/PetInfo.vue
+++ frontend/src/views/Client/PetInfo.vue
@@ -38,6 +38,56 @@
                     </div>
                 </div>
             </div>
+             <header class="mainHeader btClear gutter ">
+                <div class="mainHeaderInner">
+                                <div class="btLogoArea menuHolder btClear">
+                        <div class="port">
+                                                    <div class="btHorizontalMenuTrigger">&nbsp;<div class="bt_bb_icon" data-bt-override-class="null"><a href="#" target="_self" data-ico-fa="" class="bt_bb_icon_holder"></a></div></div>
+                                                <div class="logo">
+                                <span>
+                                    <a href="index.html"><img class="btMainLogo" data-hw="2.1111111111111" src="User/wp-content/uploads/sites/2/2019/11/logo_black.svg" alt="Buddy"></a></span>
+                            </div>
+                                                <div class="menuPort">
+                                                    <div class="topBarInMenu">
+                                <div class="topBarInMenuCell">
+                                    <div class="btButtonWidget btOutline btWithLink"><a href="#" target="_self" class="btButtonWidgetLink"><div class="btIconWidgetIcon"><span data-ico-fontawesome="" class="bt_bb_icon_holder"></span></div><div class="btIconWidgetText"><span class="btButtonWidgetText">+63 998 868 3908</span></div></a></div>						</div>
+                            </div>
+                  <nav>
+            <ul id="menu-primary-menu" class="menu">
+                <li id="menu-item-1685" class="btMenuWideDropdown menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2767 current_page_item current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-1685 btMenuWideDropdownCols-3">
+                    <div class="subToggler"></div>
+                    <a href="/" aria-current="page">Home</a>
+                </li>
+                <li id="menu-item-41" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41">
+                    <div class="subToggler"></div>
+                    <a href="/shop">Shop</a>
+                </li>
+                <li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41">
+                    <div class="subToggler"></div>
+                    <a href="/OrderHistory">My Purchases</a>
+                </li>
+                <li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41">
+                    <div class="subToggler"></div>
+                    <a href="/PetInfo">Appointment History</a>
+                </li>
+                <li id="menu-item-40" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-40">
+                    <div class="subToggler"></div>
+                    <a href="/Contact">Contact Us</a>
+                </li>
+                <li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41">
+              <div class="subToggler"></div>
+              <router-link to="#" @click="logout" class="logout-link">
+             Logout
+            </router-link>
+          </li>
+            </ul>
+        </nav>
+        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
         <div class="page-content" style="background-image: url('images/wizard-v1.jpg')">
             <div class="wizard-v1-content">
         <div class="wizard-form">
@@ -51,6 +101,15 @@
               <div class="card-body">
                 
                 <div class="mb-6">
+                   
+            </div>
+            <div class="mb-6">
+                   
+                  </div>
+                  <div class="mb-10">
+                   
+                  </div>
+            <div class="mb-6">
                    <h3>Appointment and Pet Information Form</h3>
             <p>
               To schedule an appointment and enter pet details, please fill out
@@ -353,7 +412,45 @@
       </div>
     </div>
     </div>
-
+     <div class="btSiteFooter">
+            
+            <div class="bt_bb_wrapper"><section id="bt_bb_section653e328393367" class="bt_bb_section bt_bb_layout_boxed_1200 bt_bb_vertical_align_top bt_bb_top_spacing_large bt_bb_bottom_spacing_normal" data-bt-override-class="null"><div class="bt_bb_port">
+                
+              <div class="bt_bb_cell"><div class="bt_bb_cell_inner"><div class="bt_bb_row_wrapper"><div class="bt_bb_row" data-bt-override-class="{}" data-structure="3-3-3-3"><div class="bt_bb_column col-xxl-3 col-xl-3 bt_bb_vertical_align_top bt_bb_align_left bt_bb_padding_normal bt_bb_animation_fade_in animate bt_bb_shape_inherit animated" data-width="3" data-bt-override-class="{}">
+                <div class="bt_bb_column_content"><div class="bt_bb_column_content_inner"><div class="bt_bb_image bt_bb_shape_square bt_bb_align_inherit bt_bb_hover_style_simple bt_bb_content_display_always bt_bb_content_align_middle" data-bt-override-class="null"><span><img src="User/wp-content/uploads/sites/2/2019/11/logo_black.svg" alt="Buddy" class="btLazyLoadImage btLazyLoaded"></span></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_normal" data-bt-override-class="null"></div><div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_shape_circle bt_bb_size_small bt_bb_align_inherit" style="; --icon-primary-color:#ffffff; --icon-secondary-color:#0CC0DF;" data-bt-override-class="null"><a href="https://twitter.com/bold_themes" target="_blank" data-ico-fontawesome="" class="bt_bb_icon_holder"></a></div><div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_shape_circle bt_bb_size_small bt_bb_align_inherit" style="; --icon-primary-color:#ffffff; --icon-secondary-color:#0CC0DF;" data-bt-override-class="null"><a href="https://www.instagram.com/bold_themes/" target="_blank" data-ico-fontawesome="" class="bt_bb_icon_holder"></a></div><div class="bt_bb_icon bt_bb_color_scheme_6 bt_bb_style_filled bt_bb_shape_circle bt_bb_size_small bt_bb_align_inherit" style="; --icon-primary-color:#ffffff; --icon-secondary-color:#0CC0DF;" data-bt-override-class="null"><a href="https://www.facebook.com/boldthemes/" target="_blank" data-ico-fontawesome="" class="bt_bb_icon_holder"></a></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_medium" data-bt-override-class="null"></div></div></div></div><div class="bt_bb_column col-xxl-3 col-xl-3 bt_bb_vertical_align_top bt_bb_align_left bt_bb_padding_normal bt_bb_animation_fade_in animate bt_bb_shape_inherit animated" data-width="3" data-bt-override-class="{}"><div class="bt_bb_column_content"><div class="bt_bb_column_content_inner"><header class="bt_bb_headline bt_bb_color_scheme_3 bt_bb_dash_none bt_bb_size_extrasmall bt_bb_align_inherit" style="; --primary-color:#0CC0DF; --secondary-color:#EDCCCD;" data-bt-override-class="{}"><h5 class="bt_bb_headline_tag"><span class="bt_bb_headline_content"><span>About</span></span></h5></header><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_small" data-bt-override-class="null"></div><div class="bt_bb_icon bt_bb_style_borderless bt_bb_shape_circle bt_bb_size_xsmall bt_bb_align_left" data-bt-override-class="null"><span data-ico-dripicons="" class="bt_bb_icon_holder">
+                  <span>Main: J.P Rizal St. Tawiran, Calapan City</span></span></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_small" data-bt-override-class="null"></div>
+              <div class="bt_bb_icon bt_bb_style_borderless bt_bb_shape_circle bt_bb_size_xsmall bt_bb_align_left" data-bt-override-class="null"><span data-ico-dripicons="" class="bt_bb_icon_holder"><span>+63 998 868 3908</span></span></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_small" data-bt-override-class="null"></div><div class="bt_bb_icon bt_bb_style_borderless bt_bb_shape_circle bt_bb_size_xsmall bt_bb_align_left" data-bt-override-class="null"><span data-ico-dripicons="" class="bt_bb_icon_holder"><span>Thursday - Friday - Sunday: <br><br> 9:00 AM TO 11:00 AM <br><br> 2:00 PM TO 4:00 PM</span></span></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_medium" data-bt-override-class="null"></div></div></div></div><div class="bt_bb_column col-xxl-3 col-xl-3 bt_bb_vertical_align_top bt_bb_align_left bt_bb_padding_normal bt_bb_animation_fade_in animate bt_bb_shape_inherit animated" data-width="3" data-bt-override-class="{}"><div class="bt_bb_column_content"><div class="bt_bb_column_content_inner"><header class="bt_bb_headline bt_bb_color_scheme_3 bt_bb_dash_none bt_bb_size_extrasmall bt_bb_align_inherit" style="; --primary-color:#0CC0DF; --secondary-color:#EDCCCD;" data-bt-override-class="{}"><h5 class="bt_bb_headline_tag"><span class="bt_bb_headline_content"><span>Quick Links</span></span></h5></header><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_small" data-bt-override-class="null"></div><div class="bt_bb_custom_menu bt_bb_direction_vertical"><div class="menu-quick-links-container"><ul id="menu-quick-links" class="menu"><li id="menu-item-2555" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2555"><a href="#">Veterinary Services</a></li>
+            <li id="menu-item-2556" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2556"><a href="#">Grooming Services</a></li>
+            <li id="menu-item-2557" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2557"><a href="#">Bathing Services</a></li>
+            <li id="menu-item-2558" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2558"><a href="#">Request an Appointment</a></li>
+            </ul></div></div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_medium" data-bt-override-class="null"></div></div></div></div>
+            <div class="bt_bb_column col-xxl-3 col-xl-3 bt_bb_vertical_align_top bt_bb_align_left bt_bb_padding_normal bt_bb_animation_fade_in animate bt_bb_shape_inherit animated" data-width="3" data-bt-override-class="{}"><div class="bt_bb_column_content"><div class="bt_bb_column_content_inner"><header class="bt_bb_headline bt_bb_color_scheme_3 bt_bb_dash_none bt_bb_size_extrasmall bt_bb_align_inherit" style="; --primary-color:#0CC0DF; --secondary-color:#EDCCCD;" data-bt-override-class="{}"><h5 class="bt_bb_headline_tag"><span class="bt_bb_headline_content"><span>Newsletter</span></span></h5></header><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_small" data-bt-override-class="null"></div><div class="bt_bb_contact_form_7">
+            <div class="wpcf7 js" id="wpcf7-f1539-p2767-o1" lang="en-US" dir="ltr">
+            <div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+            <form action="https://pawsitive.bold-themes.com/buddy/#wpcf7-f1539-p2767-o1" method="post" class="wpcf7-form init" aria-label="Contact form" novalidate="novalidate" data-status="init">
+            <div style="display: none;">
+            <input type="hidden" name="_wpcf7" value="1539">
+            <input type="hidden" name="_wpcf7_version" value="5.8.1">
+            <input type="hidden" name="_wpcf7_locale" value="en_US">
+            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f1539-p2767-o1">
+            <input type="hidden" name="_wpcf7_container_post" value="2767">
+            <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+            <input type="hidden" name="_wpcf7_recaptcha_response" value="">
+            </div>
+            <div class="btNewsletter">
+                <div class="btNewsletterColumn">
+                    <p><span class="wpcf7-form-control-wrap" data-name="your-email"><input size="40" class="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your email" value="" type="email" name="your-email"></span>
+                    </p>
+                </div>
+                <div class="btNewsletterButton">
+                    <p><input class="wpcf7-form-control wpcf7-submit has-spinner" type="submit" value="SUBSCRIBE"><span class="wpcf7-spinner"></span>
+                    </p>
+                </div>
+            </div><div class="wpcf7-response-output" aria-hidden="true"></div>
+            </form>
+            </div>
+            </div><div class="bt_bb_separator bt_bb_border_style_none bt_bb_bottom_spacing_medium" data-bt-override-class="null"></div></div></div></div></div></div></div></div></div></section></div><footer class="btLightSkin"></footer>
+            </div>  
         </div>
         </body> 
             </template>
@@ -395,6 +492,10 @@ export default {
     };
   },
   methods: {
+    logout() {
+      sessionStorage.removeItem('token'); // Remove the token from session storage
+      this.$router.push('/login'); // Navigate to the login page
+    },
      updateComboBox() {
         // Get the selected value from the dropdown
         var selectedTime = document.getElementById("appointment_time_select").value;
